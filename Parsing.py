565511d618bcf413cfd08136579450fda6da63bf
Prone <lautaro.milanesi@gmail.com>
2024-09-15 17:54:57 -0300

    No se, cosas

--- Parsing.py
+++ Parsing.py
@@ -1,6 +1,22 @@
 import json
 import pandas as pd
 
+import pymysql
+
+timeout = 10
+connection = pymysql.connect(
+  charset="utf8mb4",
+  connect_timeout=timeout,
+  cursorclass=pymysql.cursors.DictCursor,
+  db="defaultdb",
+  host="prone-2024-prone-2024.k.aivencloud.com",
+  password= DB_PASSWORD,
+  read_timeout=timeout,
+  port=11108,
+  user="avnadmin",
+  write_timeout=timeout,
+)
+
 # Cargar el archivo JSON
 with open('Propiedades.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
