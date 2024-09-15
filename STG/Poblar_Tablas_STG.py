01c816c95c3b015f2829a136881cb8dd9a7045a5
Prone <lautaro.milanesi@gmail.com>
2024-09-15 20:41:31 -0300

    Crear y Problar tablas en STG, falta Transacciones

---
+++ STG/Poblar_Tablas_STG.py
@@ -0,0 +1,53 @@
+import pymysql
+import pandas as pd
+
+# Configuración de la conexión a la base de datos
+timeout = 10
+connection = pymysql.connect(
+    charset="utf8mb4",
+    connect_timeout=timeout,
+    cursorclass=pymysql.cursors.DictCursor,
+    db="defaultdb",
+    host="prone-2024-prone-2024.k.aivencloud.com",
+    password= BD_pasword,
+    read_timeout=timeout,
+    port=11108,
+    user="avnadmin",
+    write_timeout=timeout,
+)
+
+# Leer el archivo Excel
+file_path = r'C:\Users\Lauta\OneDrive\Escritorio\Prone\Carga Inicial\Propiedades_Todo_en_Uno.xlsx'
+excel_file = pd.ExcelFile(file_path)
+
+# Función para insertar datos en una tabla
+def insert_into_table(cursor, table_name, df):
+    cols = ",".join([f"`{col}`" for col in df.columns])
+    placeholders = ",".join(["%s"] * len(df.columns))
+    insert_stmt = f"INSERT INTO {table_name} ({cols}) VALUES ({placeholders})"
+    data = [tuple(row) for row in df.values]
+    cursor.executemany(insert_stmt, data)
+
+# Ejecución del script para insertar datos
+try:
+    cursor = connection.cursor()
+
+    # Cargar datos de cada hoja y hacer la inserción en la base de datos
+    hojas_y_tablas = {
+        "Propiedades": "Propiedades_STG",
+        "Sucursales": "Sucursales_STG",
+        "Ubicaciones": "Ubicaciones_STG",
+        "Productores": "Productores_STG",
+        "Tipos_Propiedades": "Tipos_Propiedades_STG",
+        "Operaciones": "Operaciones_STG",
+        "Propietarios": "Propietarios_STG"
+    }
+
+    for hoja, tabla in hojas_y_tablas.items():
+        df = pd.read_excel(file_path, sheet_name=hoja)
+        insert_into_table(cursor, tabla, df)
+        connection.commit()
+        print(f"Datos insertados correctamente en la tabla {tabla}.")
+
+finally:
+    connection.close()
